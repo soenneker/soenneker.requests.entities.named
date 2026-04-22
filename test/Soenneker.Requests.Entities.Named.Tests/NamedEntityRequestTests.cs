387a0d1b7e0a9973cfee34042b26fc1c7817ef21
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:34 -0500

    TUnit conversion

--- test/Soenneker.Requests.Entities.Named.Tests/NamedEntityRequestTests.cs
+++ test/Soenneker.Requests.Entities.Named.Tests/NamedEntityRequestTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Requests.Entities.Named.Tests;
 
 public sealed class NamedEntityRequestTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
